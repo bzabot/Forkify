ce894c66a044a924c0222ea165285f330b14755f
bzabot <bruno_zabot@outlook.com>
2024-06-12 16:22:40 +0100

    pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,15 +2,16 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 // https://forkify-api.herokuapp.com/v2
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
@@ -34,20 +35,35 @@ const controlSearchResults = async function () {
   try {
     resultsView.renderSpinner();
 
+    // get search results
     const query = searchView.getQuery();
     if (!query) return;
 
+    // load search results
     await model.loadSearchResults(query);
     // console.log(model.state.search.results);
 
-    resultsView.render(model.state.search.results);
+    // Render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  // Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // Render new initial pagination buttons
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
